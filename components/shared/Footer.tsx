6352a25ecd0c07062e5b986fdc027c103582333b
abdalla <abdallashaban584@gmail.com>
2024-04-21 17:08:05 +0200

    implement footer component

---
+++ components/shared/Footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Logo from "./Logo";
+
+const Footer = () => {
+  return (
+    <footer className="flex flex-col md:flex-row items-center gap-3 md:gap-0 bg-white justify-between p-5 md:px-10">
+      <Logo />
+      <p>2024 EventSpark. &copy; All Rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
