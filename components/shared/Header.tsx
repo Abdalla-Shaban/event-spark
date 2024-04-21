0a13daf99704e44617b8c789260c20d67ca3071b
abdalla <abdallashaban584@gmail.com>
2024-04-21 17:07:42 +0200

    implement header component

---
+++ components/shared/Header.tsx
@@ -0,0 +1,38 @@
+"use client";
+import Link from "next/link";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { Button } from "../ui/button";
+import NavLinks from "./NavLinks";
+import MobileNav from "../shared/MobileNav";
+import Logo from "./Logo";
+const Header = () => {
+  return (
+    <header className="flex items-center bg-white justify-between p-5 md:px-10">
+      <Logo />
+      <SignedIn>
+        <nav className="hidden md:flex items-center gap-10">
+          <NavLinks />
+        </nav>
+      </SignedIn>
+      <SignedIn>
+        <div className="flex items-center gap-3">
+          <UserButton afterSignOutUrl="/" />
+          <div className="md:hidden flex flex-col items-center gap-10">
+            <MobileNav />
+          </div>
+        </div>
+      </SignedIn>
+      <SignedOut>
+        <Button
+          asChild
+          className="rounded-full bg-orange-500 hover:bg-orange-600"
+          size="lg"
+        >
+          <Link href="/sign-in">Login</Link>
+        </Button>
+      </SignedOut>
+    </header>
+  );
+};
+
+export default Header;
