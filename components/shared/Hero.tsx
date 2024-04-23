f0f8b23ebe7d32d29cdaa68321b118ee810f1ed7
abdalla <abdallashaban584@gmail.com>
2024-04-23 20:36:19 +0200

    fix(ui): add styles to center hero section content

--- components/shared/Hero.tsx
+++ components/shared/Hero.tsx
@@ -4,7 +4,7 @@ import heroImage from "../../public/hero.png";
 const Hero = () => {
   return (
     <section className="py-5 md:py-10">
-      <div className="p-5 flex flex-col md:flex-row gap-10 md:gap-0">
+      <div className="p-5 flex flex-col justify-center items-center md:flex-row gap-10 md:gap-0">
         <div className="flex-1 flex flex-col gap-8">
           <h1 className="text-[40px] leading-[48px] md:text-[48px] md:leading-[60px] xl:text-[58px] xl:leading-[74px] font-bold">
             Host, Connect, Celebrate: Your Events, Our Platform!
